b090be24ce93d0f303271e0fb92fa0bdad0d1705
fei419881361 <419881361@qq.com>
2017-03-15 16:46:55 +0800

    修改移动特效

--- MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DrawLogic/DrawSpecialEffects/PpgetSpecialEffects.java
+++ MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DrawLogic/DrawSpecialEffects/PpgetSpecialEffects.java
@@ -14,7 +14,7 @@ public class PpgetSpecialEffects extends SpecialEffects {
 
     int i;                  //时钟变量
     int end_time;
-    int offset;             //变成弧线偏移量
+    int[] offset;             //变成弧线偏移量
     int per_x;
     int per_y;
     Paint paint;
@@ -27,6 +27,7 @@ public class PpgetSpecialEffects extends SpecialEffects {
         end_time = 10;
         per_x = (int) ((Const.SCREENHEIGHT * 0.63 - x) / end_time);
         per_y = (int) ((Const.SCREENWIDTH * 0.93 - y) / end_time);
+        offset = new int[3];
     }
 
     @Override
@@ -45,11 +46,37 @@ public class PpgetSpecialEffects extends SpecialEffects {
     @Override
     public void docanva(Canvas canvas) {
         if (i != -1) {
-            canvas.drawCircle(x + per_x * i, y + per_y * i, (int) ((Const.CELL_WIDTH * 0.2)+i*Const.CELL_WIDTH/10 ), paint);
-            this.i = i+1;
-            Log.i("nini", "docanva: "+i+"--"+per_x+"per"+per_y);
+            paint.setColor(Color.rgb(25, 25, 200));
+
+            ChangeOffset(i);
+            canvas.drawCircle(x + per_x * i, y + per_y * i + offset[2], (int) ((Const.CELL_WIDTH * 0.2) + i * Const.CELL_WIDTH / 55), paint);
+            paint.setColor(Color.rgb(75, 75, 175));
+            canvas.drawCircle(x + per_x * (i + 1), y + per_y * i + offset[1], (int) ((Const.CELL_WIDTH * 0.2) + i * Const.CELL_WIDTH / 45), paint);
+            paint.setColor(Color.rgb(100, 100, 150));
+            canvas.drawCircle(x + per_x * (i + 2), y + per_y * i + offset[0], (int) ((Const.CELL_WIDTH * 0.2) + i * Const.CELL_WIDTH / 25), paint);
+            this.i = i + 1;
+
+            Log.i("nini", "docanva: " + i + "--" + per_x + "per" + per_y);
             if (i >= end_time)
                 i = -1;
+
         }
+
+    }
+
+    private void ChangeOffset(int i) {
+
+        if (i < end_time / 2) {
+            offset[2] = offset[1] / 2;
+            offset[1] = offset[0] / 2;
+            offset[0] += Const.CELL_HEIGHT/3;
+
+        } else {
+            offset[2] = offset[1] / 2;
+            offset[1] = offset[0] / 2;
+            offset[0] -= Const.CELL_HEIGHT / 2;
+        }
+
+
     }
 }
