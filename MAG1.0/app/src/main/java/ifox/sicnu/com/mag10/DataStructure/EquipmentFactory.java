64c50307ecc7ec22cef9932087a4faaa6d026385
URJACK <316585692@qq.com>
2017-03-21 20:43:57 +0800

    修改了装备有关项
    1·shopmanager 修改了逻辑，让其能够正常的生产装备。
    2·录入完了装备的信息。

--- MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DataStructure/EquipmentFactory.java
+++ MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DataStructure/EquipmentFactory.java
@@ -18,6 +18,20 @@ public class EquipmentFactory {
     public static Bitmap bm_sword_2;
     public static Bitmap bm_axe_1;
     public static Bitmap bm_axe_2;
+    public static Bitmap bm_shield_1;
+    public static Bitmap bm_shield_2;
+    public static Bitmap bm_hand_1;
+    public static Bitmap bm_hand_2;
+    public static Bitmap bm_up_1;
+    public static Bitmap bm_up_2;
+    public static Bitmap bm_down_1;
+    public static Bitmap bm_down_2;
+    public static Bitmap bm_belt_1;
+    public static Bitmap bm_belt_2;
+    public static Bitmap bm_shoe_1;
+    public static Bitmap bm_shoe_2;
+    public static Bitmap bm_helmet_1;
+    public static Bitmap bm_helmet_2;
 
     static {
         bm_sword_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.sword_2);
@@ -28,7 +42,34 @@ public class EquipmentFactory {
         bm_axe_1 = Bitmap.createScaledBitmap(bm_axe_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
         bm_axe_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.axe_2);
         bm_axe_2 = Bitmap.createScaledBitmap(bm_axe_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
-
+        bm_shield_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.shield_1);
+        bm_shield_1 = Bitmap.createScaledBitmap(bm_shield_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_shield_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.shield_2);
+        bm_shield_2 = Bitmap.createScaledBitmap(bm_shield_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_hand_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.hand_1);
+        bm_hand_1 = Bitmap.createScaledBitmap(bm_hand_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_hand_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.hand_2);
+        bm_hand_2 = Bitmap.createScaledBitmap(bm_hand_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_up_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.upwear_1);
+        bm_up_1 = Bitmap.createScaledBitmap(bm_up_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_up_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.upwear_2);
+        bm_up_2 = Bitmap.createScaledBitmap(bm_up_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_down_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.downwear_1);
+        bm_down_1 = Bitmap.createScaledBitmap(bm_down_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_down_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.downwear_2);
+        bm_down_2 = Bitmap.createScaledBitmap(bm_down_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_belt_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.belt_1);
+        bm_belt_1 = Bitmap.createScaledBitmap(bm_belt_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_belt_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.belt_2);
+        bm_belt_2 = Bitmap.createScaledBitmap(bm_belt_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_shoe_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.shoe_1);
+        bm_shoe_1 = Bitmap.createScaledBitmap(bm_shoe_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_shoe_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.shoe_2);
+        bm_shoe_2 = Bitmap.createScaledBitmap(bm_shoe_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_helmet_1 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.helmet_1);
+        bm_helmet_1 = Bitmap.createScaledBitmap(bm_helmet_1, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
+        bm_helmet_2 = BitmapFactory.decodeResource(Const.mContext_Game.getResources(), R.drawable.helmet_2);
+        bm_helmet_2 = Bitmap.createScaledBitmap(bm_helmet_2, Const.BAG_WIDTH, Const.BAG_HEIGHT, true);
     }
 
     public static Equipment createWeapon(String name) {
@@ -53,7 +94,8 @@ public class EquipmentFactory {
         }
         return null;
     }
-    public static Equipment createCoWeapon(String name){
+
+    public static Equipment createCoWeapon(String name) {
         if (name.equals("shield_1"))
             return createshield_1();
         else if (name.equals("shield_2"))
@@ -63,17 +105,382 @@ public class EquipmentFactory {
         return null;
     }
 
-    private static Equipment createshield_3() {
+    public static Equipment createHand(String name) {
+        if (name.equals("hand_1")) {
+            return createHand_1();
+        } else if (name.equals("hand_2")) {
+            return createHand_2();
+        } else if (name.equals("hand_3")) {
+            return createHand_3();
+        }
         return null;
+    }
+
+    public static Equipment createUpWear(String name) {
+        if (name.equals("up_1"))
+            return createUp_1();
+        else if (name.equals("up_2"))
+            return createUp_2();
+        else if (name.equals("up_3"))
+            return createUp_3();
+        return null;
+    }
+
+    public static Equipment createDownWear(String name) {
+        if (name.equals("down_1"))
+            return createDown_1();
+        else if (name.equals("down_2"))
+            return createDown_2();
+        else if (name.equals("down_3"))
+            return createDown_3();
+        return null;
+    }
+
+    public static Equipment createBelt(String name) {
+        if (name.equals("belt_1"))
+            return createBelt_1();
+        else if (name.equals("belt_2"))
+            return createBelt_2();
+        else if (name.equals("belt_3"))
+            return createBelt_3();
+        else
+            return null;
+    }
+
+    public static Equipment createShoe(String name) {
+        if (name.equals("shoe_1"))
+            return createShoe_1();
+        else if (name.equals("shoe_2"))
+            return createShoe_2();
+        else if (name.equals("shoe_3"))
+            return createShoe_3();
+        else
+            return null;
+    }
+
+    public static Equipment createHelmet(String name) {
+        if (name.equals("helmet_1"))
+            return createHelmet_1();
+        else if (name.equals("helmet_2"))
+            return createHelmet_2();
+        else if (name.equals("helmet_3"))
+            return createHelmet_3();
+        else
+            return null;
+    }
+
+
+    private static Equipment createHelmet_3() {
+        Equipment equipment = new Equipment();
+        equipment.maxHp = 80;
+
+        equipment.equipmentName = "宝盔";
+        equipment.introduce = "这是宝盔";
+        equipment.money = 40;
+        equipment.type = Equipment.HELMET;
+        equipment.bitmap = bm_helmet_2;
+
+        equipment.atm = new AttackMethod() {
+            @Override
+            public boolean attack(Unit me, Unit unit) {
+                me.hp += 3;
+                if (me.hp > me.maxHp)
+                    me.hp = me.maxHp;
+                if (unit.hp < 0)
+                    return true;
+                else
+                    return false;
+            }
+        };
+        return equipment;
+    }
+
+    private static Equipment createHelmet_2() {
+        Equipment equipment = new Equipment();
+        equipment.maxHp = 50;
+
+        equipment.equipmentName = "精良头盔";
+        equipment.introduce = "这是一条精良头盔";
+        equipment.money = 20;
+        equipment.type = Equipment.HELMET;
+        equipment.bitmap = bm_helmet_1;
+        return equipment;
+    }
+
+    private static Equipment createHelmet_1() {
+        Equipment equipment = new Equipment();
+        equipment.maxHp = 20;
+
+        equipment.equipmentName = "普通头盔";
+        equipment.introduce = "这是一条普通头盔";
+        equipment.money = 10;
+        equipment.type = Equipment.HELMET;
+        equipment.bitmap = bm_helmet_1;
+        return equipment;
+    }
+
+
+    private static Equipment createShoe_3() {
+        Equipment equipment = new Equipment();
+        equipment.dodge = (float) 0.2;
+        equipment.maxPp = 15;
+
+        equipment.equipmentName = "宝鞋";
+        equipment.introduce = "这是一条宝鞋";
+        equipment.money = 40;
+        equipment.type = Equipment.SHOE;
+        equipment.bitmap = bm_shoe_2;
+        return equipment;
+    }
+
+    private static Equipment createShoe_2() {
+        Equipment equipment = new Equipment();
+        equipment.dodge = (float) 0.15;
+        equipment.maxPp = 10;
+
+        equipment.equipmentName = "精良鞋子";
+        equipment.introduce = "这是一条精良鞋子";
+        equipment.money = 20;
+        equipment.type = Equipment.SHOE;
+        equipment.bitmap = bm_shoe_1;
+        return equipment;
+    }
+
+    private static Equipment createShoe_1() {
+        Equipment equipment = new Equipment();
+        equipment.dodge = (float) 0.1;
+        equipment.maxPp = 5;
+
+        equipment.equipmentName = "普通鞋子";
+        equipment.introduce = "这是一条普通鞋子";
+        equipment.money = 10;
+        equipment.type = Equipment.SHOE;
+        equipment.bitmap = bm_shoe_1;
+        return equipment;
+    }
+
+
+    private static Equipment createBelt_3() {
+        Equipment equipment = new Equipment();
+        equipment.resistance = (float) 0.3;
+        equipment.maxMp = 15;
+
+        equipment.equipmentName = "宝带";
+        equipment.introduce = "这是宝带";
+        equipment.money = 20;
+        equipment.type = Equipment.BELT;
+        equipment.bitmap = bm_belt_2;
+
+        equipment.atm = new AttackMethod() {
+            @Override
+            public boolean attack(Unit me, Unit unit) {
+                if (unit.hp < 0) {
+                    me.hp += unit.maxHp * 0.2;
+                    return true;
+                } else
+                    return false;
+            }
+        };
+        return equipment;
+    }
+
+    private static Equipment createBelt_2() {
+        Equipment equipment = new Equipment();
+        equipment.resistance = (float) 0.2;
+        equipment.maxMp = 10;
+
+        equipment.equipmentName = "精良腰带";
+        equipment.introduce = "这是一条精良腰带";
+        equipment.money = 20;
+        equipment.type = Equipment.BELT;
+        equipment.bitmap = bm_belt_1;
+        return equipment;
+    }
+
+    private static Equipment createBelt_1() {
+        Equipment equipment = new Equipment();
+        equipment.resistance = (float) 0.1;
+        equipment.maxMp = 5;
+
+        equipment.equipmentName = "普通腰带";
+        equipment.introduce = "这是一条普通腰带";
+        equipment.money = 10;
+        equipment.type = Equipment.BELT;
+        equipment.bitmap = bm_belt_1;
+        return equipment;
+    }
+
+
+    private static Equipment createDown_3() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 15;
+        equipment.maxHp = 35;
+
+        equipment.equipmentName = "宝裤";
+        equipment.introduce = "这是宝裤";
+        equipment.money = 40;
+        equipment.type = Equipment.WEAR_BELOW;
+        equipment.bitmap = bm_down_2;
+        return equipment;
+    }
+
+    private static Equipment createDown_2() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 10;
+        equipment.maxHp = 25;
+
+        equipment.equipmentName = "精良裤子";
+        equipment.introduce = "这是一条精良裤子";
+        equipment.money = 20;
+        equipment.type = Equipment.WEAR_BELOW;
+        equipment.bitmap = bm_down_1;
+        return equipment;
+    }
+
+    private static Equipment createDown_1() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 5;
+        equipment.maxHp = 10;
+
+        equipment.equipmentName = "普通裤子";
+        equipment.introduce = "这是一条普通裤子";
+        equipment.money = 10;
+        equipment.type = Equipment.WEAR_BELOW;
+        equipment.bitmap = bm_down_1;
+        return equipment;
+    }
+
+
+    private static Equipment createUp_3() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 30;
+        equipment.maxHp = 15;
+
+        equipment.equipmentName = "宝衣";
+        equipment.introduce = "这是宝衣";
+        equipment.money = 40;
+        equipment.type = Equipment.WEAR_UP;
+        equipment.bitmap = bm_up_2;
+        return equipment;
+    }
+
+    private static Equipment createUp_2() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 20;
+        equipment.maxHp = 10;
+
+        equipment.equipmentName = "精良上衣";
+        equipment.introduce = "这是一件精良上衣";
+        equipment.money = 20;
+        equipment.type = Equipment.WEAR_UP;
+        equipment.bitmap = bm_up_1;
+        return equipment;
+    }
+
+    private static Equipment createUp_1() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 10;
+
+        equipment.equipmentName = "普通上衣";
+        equipment.introduce = "这是一件普通上衣";
+        equipment.money = 10;
+        equipment.type = Equipment.WEAR_UP;
+        equipment.bitmap = bm_up_1;
+        return equipment;
+    }
+
+
+    private static Equipment createHand_3() {
+        Equipment equipment = new Equipment();
+        equipment.hitrate = (float) 0.15;
+        equipment.crit = (float) 0.1;
+        equipment.resistance = (float) 0.1;
+        equipment.armor = 40;
+
+        equipment.equipmentName = "血手";
+        equipment.introduce = "这是血手";
+        equipment.money = 40;
+        equipment.type = Equipment.HAND;
+        equipment.bitmap = bm_hand_2;
+
+        equipment.atm = new AttackMethod() {
+            @Override
+            public boolean attack(Unit me, Unit unit) {
+                me.hp += 3;
+                if (me.hp > me.maxHp)
+                    me.hp = me.maxHp;
+                if (unit.hp < 0)
+                    return true;
+                else
+                    return false;
+            }
+        };
+        return equipment;
+    }
+
+    private static Equipment createHand_2() {
+        Equipment equipment = new Equipment();
+        equipment.hitrate = (float) 0.15;
+        equipment.crit = (float) 0.1;
+        equipment.armor = 8;
+        equipment.resistance = (float) 0.1;
+
+        equipment.equipmentName = "精良手套";
+        equipment.introduce = "这是一把精良手套";
+        equipment.money = 20;
+        equipment.type = Equipment.HAND;
+        equipment.bitmap = bm_hand_1;
+        return equipment;
+    }
+
+    private static Equipment createHand_1() {
+        Equipment equipment = new Equipment();
+        equipment.hitrate = (float) 0.1;
+        equipment.armor = 5;
+
+        equipment.equipmentName = "普通手套";
+        equipment.introduce = "这是一把普通手套";
+        equipment.money = 10;
+        equipment.type = Equipment.HAND;
+        equipment.bitmap = bm_hand_1;
+        return equipment;
+    }
+
+
+    private static Equipment createshield_3() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 40;
+
+        equipment.equipmentName = "神盾";
+        equipment.introduce = "这是一个神盾";
+        equipment.money = 40;
+        equipment.type = Equipment.CO_WEAPON;
+        equipment.bitmap = bm_shield_2;
+        return equipment;
     }
 
     private static Equipment createshield_2() {
-        return null;
+        Equipment equipment = new Equipment();
+        equipment.armor = 25;
+
+        equipment.equipmentName = "精良盾";
+        equipment.introduce = "这是一个精良盾";
+        equipment.money = 20;
+        equipment.type = Equipment.CO_WEAPON;
+        equipment.bitmap = bm_shield_1;
+        return equipment;
     }
 
     private static Equipment createshield_1() {
+        Equipment equipment = new Equipment();
+        equipment.armor = 10;
 
-        return null;
+        equipment.equipmentName = "普通盾";
+        equipment.introduce = "这是一个普通盾";
+        equipment.money = 10;
+        equipment.type = Equipment.CO_WEAPON;
+        equipment.bitmap = bm_shield_1;
+        return equipment;
     }
 
 
@@ -143,6 +550,7 @@ public class EquipmentFactory {
         equipment.bitmap = bm_axe_1;
         return equipment;
     }
+
 
     private static Equipment createSword_5() {
         Equipment equipment = new Equipment();
--- MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DataStructure/ShopManager.java
+++ MAG1.0/app/src/main/java/ifox/sicnu/com/mag10/DataStructure/ShopManager.java
@@ -12,6 +12,8 @@ import ifox.sicnu.com.mag10.Data.Pictures;
 public class ShopManager {
     private static final String TAG = "ShopManager";
     public Context mcontext;
+    private static double span = ((double) 1) / 9;
+    Shop shop;
 
     private Pictures pictures;
     Player player;
@@ -23,16 +25,14 @@ public class ShopManager {
 
     //index 当前层数
     public Shop createShop(int index) {
-        Shop shop;
         shop = new Shop(player, mcontext);
-        setShoplist(index, shop);
+        setShoplist(index);
         return shop;
     }
 
     //通过代入floor 即，当前的层数的编号，来给传入的 shop生成对应的商品列表
-    public void setShoplist(int floor, Shop shop) {
+    public void setShoplist(int floor) {
         double random = Math.random();
-        double span = ((double) 1) / 9;
 //        if (floor < 5) {
 //            for (int i = 0; i < 8; i++) {
 //                double f = Math.random();
@@ -47,5 +47,50 @@ public class ShopManager {
 //                }
 //            }
 //        }
+        if (floor < 5) {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_1", "axe_1", "shield_1", "hand_1", "up_1", "down_1", "belt_1", "shoe_1", "helmet_1");
+        } else if (floor < 10) {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_1", "axe_2", "shield_1", "hand_1", "up_2", "down_1", "belt_2", "shoe_1", "helmet_2");
+        } else if (floor < 15) {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_2", "axe_2", "shield_2", "hand_2", "up_2", "down_2", "belt_2", "shoe_2", "helmet_2");
+        } else if (floor < 20) {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_3", "axe_3", "shield_2", "hand_2", "up_3", "down_2", "belt_3", "shoe_2", "helmet_3");
+        } else if (floor < 25) {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_4", "axe_3", "shield_2", "hand_3", "up_3", "down_3", "belt_3", "shoe_3", "helmet_3");
+        } else {
+            for (int i = 0; i < 8; i++)
+                setbylist("sword_5", "axe_4", "shield_3", "hand_3", "up_3", "down_3", "belt_3", "shoe_3", "helmet_3");
+        }
+    }
+
+    private void setbylist(String sword, String axe, String coweapon, String hand, String up, String down, String belt, String shoe, String helmet) {
+        double random = Math.random();
+        Equipment e;
+        if (random < span) {
+            e = EquipmentFactory.createWeapon(sword);
+        } else if (random < span * 2) {
+            e = EquipmentFactory.createWeapon(axe);
+        } else if (random < span * 3) {
+            e = EquipmentFactory.createCoWeapon(coweapon);
+        } else if (random < span * 4) {
+            e = EquipmentFactory.createHand(hand);
+        } else if (random < span * 5) {
+            e = EquipmentFactory.createUpWear(up);
+        } else if (random < span * 6) {
+            e = EquipmentFactory.createDownWear(down);
+        } else if (random < span * 7) {
+            e = EquipmentFactory.createBelt(belt);
+        } else if (random < span * 8) {
+            e = EquipmentFactory.createShoe(shoe);
+        } else {
+            e = EquipmentFactory.createHelmet(helmet);
+        }
+        e.addAfix(AffixFactory.createAffix());
+        shop.S_equipments.add(e);
     }
 }
